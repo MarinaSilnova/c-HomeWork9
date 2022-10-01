f9b58927e0002d6522336f5c0d4ee8dd7482366a
Marina Silnova <marusia421@mail.ru>
2022-10-01 19:41:36 +0300

    add ex68

---
+++ ex68/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+int PrintNumbers(int m, int n)
+{ 
+if (m == 0)
+    return n + 1;
+if (m > 0 && n == 0)    
+    return PrintNumbers((m - 1), 1);
+return PrintNumbers((m - 1), PrintNumbers(m, n - 1));
+}
+Console.WriteLine(PrintNumbers(3, 2));
