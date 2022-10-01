d2c22856296dafe17b79097571374412b453d860
Marina Silnova <marusia421@mail.ru>
2022-10-01 18:43:56 +0300

    add ex66

---
+++ ex66/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int PrintNumbers(int n, int m)
+{
+   
+if (n == m)
+    return m;
+return PrintNumbers(n - 1, m) + n;
+}
+
+
+Console.WriteLine(PrintNumbers(8, 4));
